f97b8496875c7c44a2a70efeb8ebc32806f85e64
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-05-21 23:12:46 +0300

    fix main react app component

--- src/index.js
+++ src/index.js
@@ -2,27 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { saveAs } from "file-saver";
 
-import "./styles.css";
-
-const centerX = 300;
-const centerY = 300;
-const radius = 200;
-const offset = 10;
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function getPointOnCircle(angle) {
-  const randomInt = getRandomInt(offset);
-  var x = centerX + (radius - randomInt) * Math.cos((angle * Math.PI) / 180);
-  var y = centerY + (radius - randomInt) * Math.sin((angle * Math.PI) / 180);
-  return { x, y, angle };
-}
-
-function findAngleRad(points) {
-  return 360 / points;
-}
+import { createBlob } from "./blob";
 
 class App extends React.Component {
   state = {
@@ -37,64 +17,23 @@ class App extends React.Component {
     this.setState({ points: this.state.points });
   };
 
-  download = (points, angle) => {
-    const transform = `skewX(${getRandomInt(10)}) skewY(${getRandomInt(
-      10
-    )}) rotate(${getRandomInt(360)} ${centerX} ${centerY})`;
-
-    const dataPath = points.map((point, index) => {
-      const nextIndex = index + 1 === points.length ? 0 : index + 1;
-      const nextPoint = points[nextIndex];
-      const mediumPoint = getPointOnCircle(point.angle + angle / 2);
-
-      if (index === 0) {
-        return `
-                  M  ${point.x} ${point.y}
-                  Q  ${mediumPoint.x} ${mediumPoint.y} ${nextPoint.x} ${
-          nextPoint.y
-        } 
-                `;
-      } else {
-        return `T ${nextPoint.x} ${nextPoint.y}`;
-      }
-    });
-    var content = `
-    <svg
-    width="300"
-    height="300"
-    viewBox="0 0 600 600"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g>
-      <path
-        d="${dataPath}"
-        transform="${transform}"
-        fill="salmon"
-      />
-    </g>
-  </svg>
-    `;
+  download = blobSVG => {
+    var content = blobSVG;
     var filename = "blob.svg";
-
     var blob = new Blob([content], {
       type: "text/plain;charset=utf-8"
     });
+
     saveAs(blob, filename);
   };
 
   render() {
     const { points } = this.state;
-    const angle = findAngleRad(points);
 
-    const pointsArray = [];
-    const randomAngle = getRandomInt(45);
-    for (let i = 0; i < points; i++) {
-      // pointsArray.push(getPointOnCircle(randomAngle + (i + 1) * angle));
-      pointsArray.push(getPointOnCircle((i + 1) * angle));
-    }
+    const blobSVG = createBlob(points);
 
     return (
-      <div className="App">
+      <div>
         <div>
           <label htmlFor="points">Complexity: </label>
           <input
@@ -105,104 +44,13 @@ class App extends React.Component {
             name="points"
             min="4"
             step="2"
-            max="20"
+            max="14"
           />
         </div>
-        <svg
-          width="300"
-          height="300"
-          viewBox="0 0 600 600"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs>
-            <linearGradient
-              id="linear-gradient"
-              x1="0%"
-              y1="0"
-              x2="100%"
-              y2="0"
-            >
-              <stop offset="0" stop-color="#43E97B">
-                <animate
-                  attributeName="stop-color"
-                  values="#43E97B;#38F9D7"
-                  dur="1s"
-                  repeatCount="indefinite"
-                />
-              </stop>
-              <stop offset=".2" stop-color="#38F9D7">
-                <animate
-                  attributeName="stop-color"
-                  values="#38F9D7;#E0C3FC"
-                  dur="1s"
-                  repeatCount="indefinite"
-                />
-              </stop>
-              <stop offset=".4" stop-color="#E0C3FC">
-                <animate
-                  attributeName="stop-color"
-                  values="#E0C3FC;#8EC5FC"
-                  dur="1s"
-                  repeatCount="indefinite"
-                />
-              </stop>
-              <stop offset=".6" stop-color="#8EC5FC">
-                <animate
-                  attributeName="stop-color"
-                  values="#8EC5FC;#4FACFE"
-                  dur="1s"
-                  repeatCount="indefinite"
-                />
-              </stop>
-              <stop offset=".8" stop-color="#4FACFE">
-                <animate
-                  attributeName="stop-color"
-                  values="#4FACFE;#00F2FE"
-                  dur="1s"
-                  repeatCount="indefinite"
-                />
-              </stop>
-              <stop offset="1" stop-color="#00F2FE">
-                <animate
-                  attributeName="stop-color"
-                  values="#00F2FE;#43E97B"
-                  dur="1s"
-                  repeatCount="indefinite"
-                />
-              </stop>
-            </linearGradient>
-          </defs>
-          <g>
-            <path
-              transform={`skewX(${getRandomInt(10)}) skewY(${getRandomInt(
-                10
-              )}) rotate(${getRandomInt(360)} ${centerX} ${centerY})`}
-              d={pointsArray.map((point, index) => {
-                const nextIndex =
-                  index + 1 === pointsArray.length ? 0 : index + 1;
-                const nextPoint = pointsArray[nextIndex];
-                const mediumPoint = getPointOnCircle(point.angle + angle / 2);
-
-                if (index === 0) {
-                  return `
-                        M  ${point.x} ${point.y}
-                        Q  ${mediumPoint.x} ${mediumPoint.y} ${nextPoint.x} ${
-                    nextPoint.y
-                  } 
-                      `;
-                } else {
-                  return `T ${nextPoint.x} ${nextPoint.y}`;
-                }
-              })}
-              fill="url(#linear-gradient)"
-            />
-          </g>
-        </svg>
+        <div dangerouslySetInnerHTML={{ __html: blobSVG }} />
         <div>
           <button onClick={this.refresh}>Refresh</button>
-          <button onClick={() => this.download(pointsArray, angle)}>
-            Download
-          </button>
+          <button onClick={() => this.download(blobSVG)}>Download</button>
         </div>
       </div>
     );
