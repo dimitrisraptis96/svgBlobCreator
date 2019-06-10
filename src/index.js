42e33578c7ae248e83a3d1dc63e39dfbda386b76
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-06-10 21:26:56 +0300

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { saveAs } from "file-saver";
+import anime from "animejs";
 
 import "./styles.css";
+import { array } from "prop-types";
 
-const centerX = 300;
-const centerY = 300;
-const radius = 200;
-const offset = 10;
+const CENTER_X = 300;
+const CENTER_Y = 300;
+const RADIUS = 200;
+const OFFSET = 10;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
 function getPointOnCircle(angle) {
-  const randomInt = getRandomInt(offset);
-  var x = centerX + (radius - randomInt) * Math.cos((angle * Math.PI) / 180);
-  var y = centerY + (radius - randomInt) * Math.sin((angle * Math.PI) / 180);
+  const randomInt = getRandomInt(OFFSET);
+  var x = CENTER_X + (RADIUS - randomInt) * Math.cos((angle * Math.PI) / 180);
+  var y = CENTER_Y + (RADIUS - randomInt) * Math.sin((angle * Math.PI) / 180);
   return { x, y, angle };
 }
 
@@ -29,18 +31,23 @@ class App extends React.Component {
     points: 4
   };
 
+  componentDidMount() {
+    this.setupAnimeJS();
+  }
+
   handlePoints = e => {
     this.setState({ points: e.target.value });
   };
 
   refresh = () => {
-    this.setState({ points: this.state.points });
+    const { points } = this.setState;
+    this.setState({ points: points });
   };
 
   download = (points, angle) => {
     const transform = `skewX(${getRandomInt(10)}) skewY(${getRandomInt(
       10
-    )}) rotate(${getRandomInt(360)} ${centerX} ${centerY})`;
+    )}) rotate(${getRandomInt(360)} ${CENTER_X} ${CENTER_Y})`;
 
     const dataPath = points.map((point, index) => {
       const nextIndex = index + 1 === points.length ? 0 : index + 1;
@@ -82,14 +89,107 @@ class App extends React.Component {
     saveAs(blob, filename);
   };
 
+  setupAnimeJS = () => {
+    anime({
+      targets: this.blob,
+      points: [
+        {
+          value: [
+            "70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369",
+            "70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369"
+          ]
+        },
+        {
+          value:
+            "70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369"
+        },
+        {
+          value:
+            "70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369"
+        },
+        {
+          value:
+            "70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369"
+        }
+      ],
+      easing: "easeOutQuad",
+      duration: 2000,
+      loop: true
+    });
+  };
+
+  getPathTransform() {
+    const skewX = `skewX(${getRandomInt(10)})`;
+    const skewY = `skewY(${getRandomInt(10)})`;
+    const rotate = `rotate(${getRandomInt(360)} ${CENTER_X} ${CENTER_Y})`;
+    return `${skewX} ${skewY} ${rotate}`;
+  }
+
+  getPathData(pointsArray, angle) {
+    const numOfPoints = pointsArray.length;
+
+    return pointsArray.map((point, index) => {
+      const nextIndex = index + 1 === numOfPoints ? 0 : index + 1;
+      const nextPoint = pointsArray[nextIndex];
+      const mediumPoint = getPointOnCircle(point.angle + angle / 2);
+
+      if (index === 0) {
+        return `
+              M  ${point.x} ${point.y}
+              Q  ${mediumPoint.x} ${mediumPoint.y} ${nextPoint.x} ${
+          nextPoint.y
+        } 
+            `;
+      } else {
+        return `T ${nextPoint.x} ${nextPoint.y}`;
+      }
+    });
+  }
+
+  getPath(points) {
+    const angle = findAngleRad(points);
+    const pointsArray = this.calculatePoints(points, angle);
+
+    return (
+      <path
+        transform={this.getPathTransform()}
+        d={this.getPathData(pointsArray, angle)}
+        fill="url(#linear-gradient)"
+      />
+    );
+  }
+
+  calculatePoints(points, angle) {
+    const pointsArray = [];
+    for (let i = 0; i < points; i++) {
+      pointsArray.push(getPointOnCircle((i + 1) * angle));
+    }
+    return pointsArray;
+  }
+
+  getRandomGradientColors() {
+    return ["#43E97B", "#38F9D7"];
+  }
+
+  getLinearGradient() {
+    const colors = this.getRandomGradientColors();
+    const numOfColors = colors.length;
+    const factor = 1 / (numOfColors - 1);
+    return (
+      <linearGradient id="linear-gradient">
+        {colors.map((color, index) => (
+          <stop offset={factor * index} stop-color={color} />
+        ))}
+      </linearGradient>
+    );
+  }
+
   render() {
     const { points } = this.state;
     const angle = findAngleRad(points);
 
     const pointsArray = [];
-    const randomAngle = getRandomInt(45);
     for (let i = 0; i < points; i++) {
-      // pointsArray.push(getPointOnCircle(randomAngle + (i + 1) * angle));
       pointsArray.push(getPointOnCircle((i + 1) * angle));
     }
 
@@ -109,42 +209,14 @@ class App extends React.Component {
           />
         </div>
         <svg
+          ref={ref => (this.blob = ref)}
           width="300"
           height="300"
           viewBox="0 0 600 600"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <defs>
-            <linearGradient id="linear-gradient">
-              <stop offset="0" stop-color="#43E97B" />
-              <stop offset="1" stop-color="#38F9D7" />
-            </linearGradient>
-          </defs>
-          <g>
-            <path
-              transform={`skewX(${getRandomInt(10)}) skewY(${getRandomInt(
-                10
-              )}) rotate(${getRandomInt(360)} ${centerX} ${centerY})`}
-              d={pointsArray.map((point, index) => {
-                const nextIndex =
-                  index + 1 === pointsArray.length ? 0 : index + 1;
-                const nextPoint = pointsArray[nextIndex];
-                const mediumPoint = getPointOnCircle(point.angle + angle / 2);
-
-                if (index === 0) {
-                  return `
-                        M  ${point.x} ${point.y}
-                        Q  ${mediumPoint.x} ${mediumPoint.y} ${nextPoint.x} ${
-                    nextPoint.y
-                  } 
-                      `;
-                } else {
-                  return `T ${nextPoint.x} ${nextPoint.y}`;
-                }
-              })}
-              fill="url(#linear-gradient)"
-            />
-          </g>
+          <defs>{this.getLinearGradient()}</defs>
+          <g>{this.getPath(points)}</g>
         </svg>
         <div>
           <button onClick={this.refresh}>Refresh</button>
